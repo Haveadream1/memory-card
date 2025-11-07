1a9524f95ba9b44aee3e2658e8e4a69f74b79d5d
Haveadream1 <260602david@gmail.com>
2025-11-07 09:29:53 +0100

    feat: import scoreDiv component to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
+import ScoreDiv from "./ScoreDiv";
+
 export default function Header() {
     return (
         <header>
             <h1>Memory card</h1>
+            <ScoreDiv 
+                divClassName="score-container" 
+                spanClassName="bold"
+            />
         </header>
     );
 }
