480b2f6faa8567268f65b33eff6e62b6e50f2767
Haveadream1 <260602david@gmail.com>
2025-11-08 21:10:26 +0100

    feat: handle main logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,92 @@
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import CardContainer from "./containers/CardsContainer";
 import './App.css'
 
+
 export default function App() {
+    const [imageUrls, setImageUrls] = useState([]); // Init with an empty array
+    const [selectedImage, setSelectedImage] = useState();
+    const [score, setScore] = useState(0);
+    const [highScore, setHighScore] = useState(0);
+    const [pastRandomUrl, setPastRandomUrl] = useState(null);
+
+    async function getImage(nameOrId) {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${nameOrId}/`);
+            const data = await response.json();
+
+            const imageUrl = data.sprites.front_default; // Access the image with the JSON path
+            return imageUrl
+        } catch (error) {
+            console.error("An error occurred while fetching data:", error);
+            throw error;
+        }
+    }
+
+    const selectRandomUrl = (randomIndexArr) => {
+        const selectedIndex = Math.floor(Math.random() * randomIndexArr.length);
+        const randomUrl = randomIndexArr[selectedIndex];
+
+        return randomUrl;
+    }
+
+    const min = 1;
+    const max = 20;
+
+    useEffect(() => {
+        async function loadImages() {
+            const randomIndexArr = [];
+            const setIndex = new Set();
+
+            if (pastRandomUrl !== null) {
+                setIndex.add(pastRandomUrl);
+                randomIndexArr.push(pastRandomUrl);
+
+                console.log(pastRandomUrl);
+            }
+
+            while (randomIndexArr.length < 4) { // TODO add a placeholder for 4 so we can easily modify difficulty
+                let randomIndex = Math.floor(Math.random() * (max - min) + min);
+                if (!setIndex.has(randomIndex)) { // Check if the index is unique
+                    setIndex.add(randomIndex);
+                    randomIndexArr.push(randomIndex);
+                }
+            }
+
+            console.log(randomIndexArr);
+
+            const newPastUrl =  selectRandomUrl(randomIndexArr); // Update state value
+            setPastRandomUrl(newPastUrl);
+
+            const urls = await Promise.all( // Wait for all the images to be loaded
+                randomIndexArr.map(index => getImage(index))
+            );
+            setImageUrls(urls) // Set the state with the ulrs fetched
+        }
+        loadImages();
+    },[score]); // [] to run only when the button is clicked
+    
+    // console.log(randomIndexArr)
+
+    const handleClickButton = () => {
+        setScore((score) => score + 1);
+    }
+    
     return (
         <>
-            <Header />
-            <CardContainer />
+            <Header score={score} />
+            <CardContainer imageUrls={imageUrls} onClick={handleClickButton} />
         </>
     );
 }
 
+// TODO : make the card spin or full blue after each click
 
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     // <>
-//     //   <div>
-//     //     <a href="https://vite.dev" target="_blank">
-//     //       <img src={viteLogo} className="logo" alt="Vite logo" />
-//     //     </a>
-//     //     <a href="https://react.dev" target="_blank">
-//     //       <img src={reactLogo} className="logo react" alt="React logo" />
-//     //     </a>
-//     //   </div>
-//     //   <h1>Vite + React</h1>
-//     //   <div className="card">
-//     //     <button onClick={() => setCount((count) => count + 1)}>
-//     //       count is {count}
-//     //     </button>
-//     //     <p>
-//     //       Edit <code>src/App.jsx</code> and save to test HMR
-//     //     </p>
-//     //   </div>
-//     //   <p className="read-the-docs">
-//     //     Click on the Vite and React logos to learn more
-//     //   </p>
-//     // </>
-//   )
-// }
-
-// export default App
+            // while (randomIndexArr.length < 4) {
+            //     const randomIndex = Math.floor(Math.random() * (max - min) + min);
+            //     if (!setIndex.has(randomIndex)) { // Check if the index is unique
+            //         setIndex.add(randomIndex);
+            //         randomIndexArr.push(randomIndex);
+            //     }
+            // }
