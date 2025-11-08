997c393404696698cb9a651bf92e584cbe9b538a
Haveadream1 <260602david@gmail.com>
2025-11-08 21:09:43 +0100

    feat: pass score prop to component

--- src/components/ScoreDiv.jsx
+++ src/components/ScoreDiv.jsx
@@ -1,11 +1,12 @@
 export default function ScoreDiv({
     divClassName,
     spanClassName,
+    score
 }) {
     return (
         <div className={divClassName}>
-            <p>Score: <span className={spanClassName}>10</span></p>
+            <p>Score: <span className={spanClassName}>{score}</span></p>
             <p>High score: <span className={spanClassName}>20</span></p>
         </div>
     );
-}
+}   
