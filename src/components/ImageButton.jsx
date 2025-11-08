22388379344270dbda85d8740e010a3d4ccd44b1
Haveadream1 <260602david@gmail.com>
2025-11-08 21:10:13 +0100

    feat: handle imageButton component

--- src/components/ImageButton.jsx
+++ src/components/ImageButton.jsx
@@ -1,7 +1,20 @@
-export default function ImageButton() {
+export default function ImageButton({
+    src,
+    buttonId,
+    onClick,
+}) {
+    const handleOnClick = (e) => {
+        onClick();
+        console.log(e.target.querySelector("img").src)
+    }
+
     return (
-        <button className="card">
-            <p>Test</p>
+        <button className="card" onClick={handleOnClick}>
+            <img src={src} alt={buttonId} />
         </button>
     );
 }
+// TODO: add an alt for the image
+// TODO: take off buttonId after
+
+{/* <p>{count}</p> */}
