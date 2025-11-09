aa4bbf7f33622de9209ae3a589103c33d1d53111
Haveadream1 <260602david@gmail.com>
2025-11-09 10:02:03 +0100

    feat: pass score prop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
 import ScoreDiv from "./ScoreDiv";
 
-export default function Header() {
+export default function Header({
+    score
+}) {
     return (
         <header>
             <h1>Memory card</h1>
             <ScoreDiv 
                 divClassName="score-container" 
                 spanClassName="bold"
+                score={score}
             />
         </header>
     );
